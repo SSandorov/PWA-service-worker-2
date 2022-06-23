0f75528c239c304c769af9bcb03c805db448e56c
ssandorov <stefansandorovssb@gmail.com>
2022-06-23 15:29:26 +0200

    SW: instalación, activación y event.waitUntil()

--- js/app.js
+++ js/app.js
@@ -2,5 +2,18 @@
 
 // Detectar si podemos usar Service Workers
 if ( navigator.serviceWorker ) {
+    /*
+    si puede usuar el service worker, lo registrará con el siguiente comanda que especifica
+    la ruta del service worker
+
+    Sino existe ningún service worker anterior, este comando va a
+        - descargar
+        - instalar
+        - y activar
+    el service worker
+
+    en cada uno de estos procesos podemos añadir un evento para controlar cómo se 
+    gestiona la información
+    */
     navigator.serviceWorker.register('/sw.js');
 }
--- sw.js
+++ sw.js
@@ -1,3 +1,54 @@
-
 // Ciclo de vida del SW
 
+/*
+Como está comentado en el app.js, cuando se registra por primera vez un service worker,
+este se descarga, instala y activa
+Y en cada uno de estos momentos, se puede crear un evento que maneje la información del
+proceso
+*/
+
+// evento que maneja la instalación del sw
+self.addEventListener('install', event => {
+    /*
+    Durante el proceso de instalación se llevan a cabo tareas como:
+        - Descargar los archivos que componen mi aplicación
+        - Creamos un caché
+    */
+    // cada vez que haya un cambio en el service worker, se instalará un sw nuevo 
+    console.log('Se instaló el SW');
+
+    // para que la activación del sw surta efecto inmediatamente sin que el usuario
+    // tenga que recargar la página, escribimos el siguiente código en la instalación
+    // es importante saber que son buenas prácticas el no hacer esto
+    // self.skipWaiting();
+
+    /*
+    Por desgracia, cuando se instala el sw, inmediatamente despúes se puede activar el sw,
+    para evitar errores en caso de que hayan instalaciones pendientes después de la 
+    activación del sw, empleamos el event.waitUntil(). Es importante saber que este método
+    recibe una promesa
+    
+    Para poder comprobar esta situación, empleamos un setTimeOut() que hace el efecto de
+    instalación pendiente. Cuando esto sucede sin el waitUntil(), la activación sucede antes
+    de que el setTimeOut() termine. Pero de esta manera podemos manejar el desfase
+    */
+    const instAdicional = new Promise((res, rej) => {
+        setTimeout(() => {
+            console.log('SW: Instalaciones terminadas');
+            self.skipWaiting();
+            res();
+        }, 1000)
+    }); 
+
+    event.waitUntil(instAdicional);
+})
+
+// evento que maneja la activación del sw
+// la activación supone el momento en el que el sw toma el control de la página web
+self.addEventListener('activate', event => {
+    /*
+    Durante el proceso de activación se llevan a cabo tareas como:
+        - Borrar el caché viejo
+    */
+    console.log('Se activó el sw');
+});
